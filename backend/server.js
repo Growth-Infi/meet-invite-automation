f1bbe4aa5c76b5bf5983d76c8f551dcd22d6b41b
Vedant <vedantdeshmukh3108@gmail.com>
2026-04-20 12:17:37 +0530

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   }),
+// );
 app.use("/gmail", gmailRoutes);
 app.use("/campaign", campaignRoutes);
 
